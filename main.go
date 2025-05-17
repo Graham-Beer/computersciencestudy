2e27bc65456de9e89368ee6011015909c3775955
Graham Beer <Graham.Beer@outlook.com>
2025-05-17 12:06:48 +0100

    corrected 'post' path to 'posts' in vercel.json

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "net/http"
-    "computersciencestudy/api"
-    "github.com/gorilla/mux"
+	"computersciencestudy/api"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/api/posts", api.Handler).Methods("GET")
-    r.HandleFunc("/api/posts/{slug}", api.Handler).Methods("GET")
-    r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
-    http.ListenAndServe(":8080", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/posts", api.Handler).Methods("GET")
+	r.HandleFunc("/api/posts/{slug}", api.Handler).Methods("GET")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8080", r)
 }
