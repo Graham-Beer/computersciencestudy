16e0f88fdb04a72cdb9567e0437a7b62c981057e
Graham Beer <Graham.Beer@outlook.com>
2025-05-17 12:56:33 +0100

    amended vercel.json

--- database/data.sql
+++ database/data.sql
@@ -1,23 +1,15 @@
 --
 -- PostgreSQL database dump
 --
-
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+INSERT INTO
+    posts (title, content, author, created_at, slug)
+VALUES
+    (
+        'Understanding Algorithms',
+        'This is a blog post about algorithms...',
+        'John Doe',
+        CURRENT_TIMESTAMP,
+        'understanding-algorithms'
+    );
 
 --
--- PostgreSQL database dump complete
---
-
--- database/schema.sql
+++ database/schema.sql
@@ -1,23 +1,34 @@
 --
 -- PostgreSQL database dump
 --
-
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.4
+--
+-- PostgreSQL database schema
+--
+-- Set client encoding
+SET
+    client_encoding = 'UTF8';
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+SET
+    standard_conforming_strings = on;
+
+SET
+    client_min_messages = warning;
+
+-- Create posts table
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    slug VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Create index on slug for faster lookups
+CREATE INDEX idx_posts_slug ON posts (slug);
 
 --
 -- PostgreSQL database dump complete
 --
-
